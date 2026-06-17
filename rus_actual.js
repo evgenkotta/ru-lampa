34fe973b5cf63892af52e459cdcfc52710e01d6d
Evgenii <59400117+evgenkotta@users.noreply.github.com>
2026-06-17 19:48:30 +0300

    Update rus_actual.js with new settings and functions
    Refactor plugin initialization and add settings for movies and TV shows.

--- rus_actual.js
+++ rus_actual.js
@@ -2,90 +2,270 @@
 (function () {
     'use strict';
 
-    if (window.rus_actual_loaded) return;
-    window.rus_actual_loaded = true;
+    if (window.__ru_actual_plugin) return;
+    window.__ru_actual_plugin = true;
 
-    function createRow(type, title) {
-        return {
-            name: 'ru_' + type,
-            title: title,
+    function waitForLampa(callback) {
+        if (window.Lampa) callback();
+        else setTimeout(function () {
+            waitForLampa(callback);
+        }, 100);
+    }
+
+    waitForLampa(function () {
+
+        function setting(name, def) {
+            return Lampa.Storage.get(name, def);
+        }
+
+        function registerSettings() {
+            if (!Lampa.SettingsApi) return;
+
+            Lampa.SettingsApi.addComponent({
+                component: 'ru_actual',
+                name: 'Русские фильмы и сериалы',
+                icon: '<svg viewBox="0 0 24 24"><path d="M12 2L2 7v10l10 5 10-5V7z"/></svg>'
+            });
+
+            Lampa.SettingsApi.addParam({
+                component: 'ru_actual',
+                param: {
+                    name: 'ru_actual_movies',
+                    type: 'trigger',
+                    default: true
+                },
+                field: {
+                    name: 'Показывать фильмы'
+                }
+            });
+
+            Lampa.SettingsApi.addParam({
+                component: 'ru_actual',
+                param: {
+                    name: 'ru_actual_tv',
+                    type: 'trigger',
+                    default: true
+                },
+                field: {
+                    name: 'Показывать сериалы'
+                }
+            });
+
+            Lampa.SettingsApi.addParam({
+                component: 'ru_actual',
+                param: {
+                    name: 'ru_actual_animation',
+                    type: 'trigger',
+                    default: false
+                },
+                field: {
+                    name: 'Показывать мультфильмы'
+                }
+            });
+
+            Lampa.SettingsApi.addParam({
+                component: 'ru_actual',
+                param: {
+                    name: 'ru_actual_russian_only',
+                    type: 'trigger',
+                    default: true
+                },
+                field: {
+                    name: 'Только русский язык'
+                }
+            });
+
+            Lampa.SettingsApi.addParam({
+                component: 'ru_actual',
+                param: {
+                    name: 'ru_actual_votes',
+                    type: 'select',
+                    values: {
+                        0: '0',
+                        5: '5',
+                        10: '10',
+                        20: '20',
+                        50: '50'
+                    },
+                    default: 20
+                },
+                field: {
+                    name: 'Минимум голосов'
+                }
+            });
+
+            Lampa.SettingsApi.addParam({
+                component: 'ru_actual',
+                param: {
+                    name: 'ru_actual_sort',
+                    type: 'select',
+                    values: {
+                        popularity: 'Популярные',
+                        release: 'Новые',
+                        rating: 'По рейтингу'
+                    },
+                    default: 'popularity'
+                },
+                field: {
+                    name: 'Сортировка'
+                }
+            });
+
+            Lampa.SettingsApi.addParam({
+                component: 'ru_actual',
+                param: {
+                    name: 'ru_actual_limit',
+                    type: 'select',
+                    values: {
+                        10: '10',
+                        20: '20',
+                        30: '30'
+                    },
+                    default: 20
+                },
+                field: {
+                    name: 'Количество карточек'
+                }
+            });
+        }
+
+        function buildMovieUrl() {
+            var url =
+                'discover/movie' +
+                '?watch_region=RU' +
+                '&with_origin_country=RU';
+
+            if (setting('ru_actual_russian_only', true)) {
+                url += '&with_original_language=ru';
+            }
+
+            if (!setting('ru_actual_animation', false)) {
+                url += '&without_genres=16';
+            }
+
+            url +=
+                '&vote_count.gte=' +
+                setting('ru_actual_votes', 20);
+
+            switch (setting('ru_actual_sort', 'popularity')) {
+                case 'release':
+                    url += '&sort_by=primary_release_date.desc';
+                    break;
+
+                case 'rating':
+                    url += '&sort_by=vote_average.desc';
+                    break;
+
+                default:
+                    url += '&sort_by=popularity.desc';
+            }
+
+            return url;
+        }
+
+        function buildTvUrl() {
+            var url =
+                'discover/tv' +
+                '?watch_region=RU' +
+                '&with_origin_country=RU';
+
+            if (setting('ru_actual_russian_only', true)) {
+                url += '&with_original_language=ru';
+            }
+
+            if (!setting('ru_actual_animation', false)) {
+                url += '&without_genres=16';
+            }
+
+            url +=
+                '&vote_count.gte=' +
+                setting('ru_actual_votes', 20);
+
+            switch (setting('ru_actual_sort', 'popularity')) {
+                case 'release':
+                    url += '&sort_by=first_air_date.desc';
+                    break;
+
+                case 'rating':
+                    url += '&sort_by=vote_average.desc';
+                    break;
+
+                default:
+                    url += '&sort_by=popularity.desc';
+            }
+
+            return url;
+        }
+
+        function createRow(title, url) {
+            return function (ready) {
+                Lampa.Api.list(
+                    {
+                        source: 'tmdb',
+                        url: url
+                    },
+                    function (json) {
+                        json = json || {};
+                        json.results = json.results || [];
+
+                        var limit = Number(
+                            setting('ru_actual_limit', 20)
+                        );
+
+                        json.results = json.results.slice(0, limit);
+
+                        json.title = title;
+                        json.name = title;
+
+                        ready(json);
+                    },
+                    function () {
+                        ready({
+                            title: title,
+                            name: title,
+                            results: []
+                        });
+                    }
+                );
+            };
+        }
+
+        registerSettings();
+
+        Lampa.ContentRows.add({
+            name: 'ru_actual_movies',
+            title: 'Русские фильмы',
             screen: ['main'],
             index: -1000,
 
             call: function () {
-                return function (call) {
-                    var url;
+                if (!setting('ru_actual_movies', true))
+                    return [];
 
-                    if (type === 'movie') {
-                        url =
-                            'discover/movie' +
-                            '?sort_by=popularity.desc' +
-                            '&with_origin_country=RU' +
-                            '&with_original_language=ru' +
-                            '&without_genres=16' +
-                            '&vote_count.gte=10';
-                    }
-                    else {
-                        url =
-                            'discover/tv' +
-                            '?sort_by=popularity.desc' +
-                            '&with_origin_country=RU' +
-                            '&with_original_language=ru' +
-                            '&without_genres=16,10764,10767' +
-                            '&vote_count.gte=10';
-                    }
-
-                    Lampa.Api.list(
-                        {
-                            source: 'tmdb',
-                            url: url
-                        },
-                        function (json) {
-                            if (!json || !json.results || !json.results.length) {
-                                return call();
-                            }
-
-                            json.title = title;
-
-                            json.results = json.results
-                                .filter(function (item) {
-                                    return item.poster_path;
-                                })
-                                .filter(function (item) {
-                                    return item.overview;
-                                })
-                                .slice(0, 20);
-
-                            if (!json.results.length) {
-                                return call();
-                            }
-
-                            json.results.forEach(function (item) {
-                                item.promo = item.overview;
-                                item.promo_title =
-                                    item.title ||
-                                    item.name ||
-                                    '';
-                            });
-
-                            call(json);
-                        },
-                        function () {
-                            call();
-                        }
-                    );
-                };
+                return createRow(
+                    'Русские фильмы',
+                    buildMovieUrl()
+                );
             }
-        };
-    }
+        });
 
-    Lampa.ContentRows.add(
-        createRow('movie', 'Русские фильмы')
-    );
+        Lampa.ContentRows.add({
+            name: 'ru_actual_tv',
+            title: 'Русские сериалы',
+            screen: ['main'],
+            index: -999,
 
-    Lampa.ContentRows.add(
-        createRow('tv', 'Русские сериалы')
-    );
+            call: function () {
+                if (!setting('ru_actual_tv', true))
+                    return [];
 
-    console.log('[Rus Actual] loaded');
+                return createRow(
+                    'Русские сериалы',
+                    buildTvUrl()
+                );
+            }
+        });
+
+        console.log('[Ru Actual] loaded');
+    });
 })();
